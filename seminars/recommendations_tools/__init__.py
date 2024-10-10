242e5adb8ca158d034ed990887faab18b1a1efbb
deniskirbaba <kirbabadenis@gmail.com>
2024-10-10 13:33:53 +0300

    done seminars for collaborative filtering

---
+++ seminars/recommendations_tools/__init__.py
@@ -0,0 +1,11 @@
+from .recommendations_tools import (
+    calculate_similar_items,
+    get_recommendations,
+    get_recommended_items,
+    load_movie_lens,
+    sim_distance,
+    sim_pearson,
+    similar_films,
+    top_matches,
+    transform_prefs,
+)
